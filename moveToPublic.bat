b0473e4c1444c4f4a7396347dc3b3661b4ccbcd4
Shlomi Zevin <shazbak@gmail.com>
2025-03-21 11:51:25 +0200

    first deployed version

---
+++ moveToPublic.bat
@@ -0,0 +1,23 @@
+@echo off
+echo Creating 'public' directory if not exists...
+if not exist public mkdir public
+
+echo Copying files to 'public'...
+copy index.html public\
+copy rulebook.html public\
+copy strategy.html public\
+copy cardtypes.html public\
+copy faq.html public\
+
+copy index.css public\
+copy rulebook.css public\
+copy strategy.css public\
+copy cardtypes.css public\
+
+copy script.js public\
+
+echo Copying assets folder...
+xcopy /E /I assets public\assets\
+
+echo All files copied. Ready for deployment!
+pause
