a9e1bfa28ab41153f182ec5f61d51692af06d57a
Shlomi Zevin <shazbak@gmail.com>
2025-03-24 15:58:14 +0200

    fix move script

--- moveToPublic.bat
+++ moveToPublic.bat
@@ -4,12 +4,14 @@ if not exist public mkdir public
 
 echo Copying files to 'public'...
 copy index.html public\
+copy index-heb.html public\
 copy rulebook.html public\
 copy strategy.html public\
 copy cardtypes.html public\
 copy faq.html public\
 
 copy index.css public\
+copy index-heb.css public\
 copy rulebook.css public\
 copy strategy.css public\
 copy cardtypes.css public\
