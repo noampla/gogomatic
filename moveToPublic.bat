96572010dfb8b58a1a6058caaac396e0bd7204b6
Shlomi Zevin <shazbak@gmail.com>
2025-03-27 00:15:28 +0200

    add all pages in heb

--- moveToPublic.bat
+++ moveToPublic.bat
@@ -6,8 +6,11 @@ echo Copying files to 'public'...
 copy index.html public\
 copy index-heb.html public\
 copy rulebook.html public\
+copy rulebook-heb.html public\
 copy strategy.html public\
+copy strategy-heb.html public\
 copy cardtypes.html public\
+copy cardtypes-heb.html public\
 copy faq.html public\
 
 copy index.css public\
